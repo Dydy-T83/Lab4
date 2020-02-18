4af99223e9a8982b6be1fe81243293aa67ddf475
Rudy TOMATIS - STUDENT <G00387786@gmit.ie>
2020-02-18 17:48:10 +0000

    first commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -7,4 +7,20 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'data-binding-app';
+  count:number =0;
+  hideLabel:boolean=true;
+
+  buttonPressed(){
+    console.log("Button pressed");
+    this.count++;
+  }
+
+  imageCliked(){
+    console.log("Button pressed");
+    if(this.hideLabel){
+      this.hideLabel=false;
+    }else{
+      this.hideLabel=true;
+    }
+  }
 }
